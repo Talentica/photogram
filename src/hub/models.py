ba53251df047abcf6ac16c09a803ae3e7a0e24e7
Toran Sahu <toran.sahu@yahoo.com>
2018-10-31 16:14:28 +0530

    [added] fields to Photo Model

--- src/hub/models.py
+++ src/hub/models.py
@@ -1,3 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-10-31 15:38
+
+"""
+models.py
+"""
+
 from django.db import models
 
-# Create your models here.
+
+__author__ = 'Toran Sahu <toran.sahu@yahoo.com>'
+__license__ = 'Distributed under terms of the MIT license'
+
+
+class Photo(models.Model):
+    """Photo Collection Model"""
+    image = models.ImageField()
+    title = models.CharField(max_length=256)
+    token = models.CharField(max_legth=512)
+    uploaded_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-uploaded_at')
+
+    def __str__(self):
+        return self.title
+
+
+
