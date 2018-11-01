872d2c7aa3efdfbc23a2102c40206cfd3e0db192
Toran Sahu <toran.sahu@yahoo.com>
2018-11-01 10:00:16 +0530

    [new] added urls for Photo viewsets

---
+++ src/hub/urls.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-11-01 09:41
+
+"""
+urls.py
+"""
+
+
+from rest_framework.routers import DefaultRouter
+from hub.views import PhotoViewSet
+
+
+__author__ = 'Toran Sahu  <toran.sahu@yahoo.com>'
+__license__ = 'Distributed under terms of the MIT license.'
+
+
+router = DefaultRouter()
+router.register('', PhotoViewSet)
+
+urlpatterns = router.urls
