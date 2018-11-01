136b5ea8470a3451c6ee61f22fdb37432fe705f2
Toran Sahu <toran.sahu@yahoo.com>
2018-11-01 15:42:49 +0530

    [new] site level settings file

---
+++ src/photogram/settings.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-11-01 15:26
+
+"""
+settings.py
+"""
+
+
+import socket
+
+
+__author__ = 'Toran Sahu <toran.sahu@yahoo.com>'
+__license__ = 'Distributed under terms of the MIT license'
+
+
+if socket.gethostname() in ('ToranS-UB', ):
+    from photogram.base_settings import *
