d9b9d2e55a4325b9d08cd6af088ce1f7ecc6cef2
Toran Sahu <toran.sahu@yahoo.com>
2018-11-01 20:10:50 +0530

    [update] added blank attrbt to owner field

--- src/hub/models.py
+++ src/hub/models.py
@@ -8,6 +8,7 @@ models.py
 """
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
 __author__ = 'Toran Sahu <toran.sahu@yahoo.com>'
@@ -16,13 +17,14 @@ __license__ = 'Distributed under terms of the MIT license'
 
 class Photo(models.Model):
     """Photo Collection Model"""
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     image = models.ImageField()
     title = models.CharField(max_length=256)
     uploaded_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-       ordering = ('-uploaded_at', )
+        ordering = ('-uploaded_at', )
 
     def __str__(self):
-        return self.title
+        return str(self.title)
