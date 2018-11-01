8e4276292f541e75f0fbdac010b99ccdf0b84e8c
Toran Sahu <toran.sahu@yahoo.com>
2018-11-01 20:11:39 +0530

    [updated] serializer to accept owner field

--- src/hub/serializers.py
+++ src/hub/serializers.py
@@ -7,18 +7,24 @@
 serializers.py
 """
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers 
 from hub.models import Photo
+from django.contrib.auth.models import User
 
 
 __author__ = 'Toran Sahu  <toran.sahu@yahoo.com>'
 
 
-class PhotoSerializer(ModelSerializer):
+class PhotoSerializer(serializers.ModelSerializer):
     """Photo Serializer Class"""
+#     owner = serializers.HiddenField(
+#        default=serializers.CurrentUserDefault()
+#     )
+    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(),
+              default=serializers.CurrentUserDefault()
+    )
 
     class Meta:
         model = Photo
-        fields = '__all__'
-
-
+        fields = "__all__"
+        read_only_fields = ('uploaded_at', 'updated_at')
