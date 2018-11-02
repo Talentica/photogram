daaaa184e5f0b59f8e6fdf9e83b01973f4b274b0
Toran Sahu <toran.sahu@yahoo.com>
2018-11-02 18:32:42 +0530

    [added] get_shared() action view to access shared item

--- src/hub/views.py
+++ src/hub/views.py
@@ -10,12 +10,13 @@ views.py
 from rest_framework import viewsets
 from hub.models import Photo
 from hub.serializers import PhotoSerializer
-from photogram.permissions import IsUser, IsAuthenticatedOrReadOnly
+from photogram.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
-__author__ = 'Toran Sahu  <toran.sahu@yahoo.com>'
-__license__ = 'Distributed under terms of the MIT license.'
+__author__ = "Toran Sahu  <toran.sahu@yahoo.com>"
+__license__ = "Distributed under terms of the MIT license."
 
 
 class PhotoViewSet(viewsets.ModelViewSet):
@@ -24,9 +25,7 @@ class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
     # TODO: fix to not pass anonymous user to serializer
-    permission_classes = (
-            IsAuthenticatedOrReadOnly,
-    )
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def list(self, request, *args, **kwargs):
         global permission_classes
@@ -34,7 +33,9 @@ class PhotoViewSet(viewsets.ModelViewSet):
         if self.request.user.is_superuser or self.request.user.is_staff:
             queryset = self.filter_queryset(self.get_queryset())
         else:
-            queryset = self.filter_queryset(self.queryset.filter(owner=self.request.user))
+            queryset = self.filter_queryset(
+                self.queryset.filter(owner=self.request.user)
+            )
 
         page = self.paginate_queryset(queryset)
         if page is not None:
@@ -42,4 +43,24 @@ class PhotoViewSet(viewsets.ModelViewSet):
             return self.get_paginated_response(serializer.data)
 
         serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["GET"],
+        detail=False,
+        url_path="^token/(?P<token>[a-zA-Z0-9]+)",
+        url_name="get-shared",
+        permission_classes=[],
+    )
+    def get_shared(self, request, token):
+        """Get shared image"""
+        # TODO: check for valid token (JWT) in db,fetch image id against token
+        photo = Photo.objects.filter(id=32)  # TODO: hardcoded yet, get id from token
+
+        page = self.paginate_queryset(photo)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(photo, many=True)
         return Response(serializer.data)
