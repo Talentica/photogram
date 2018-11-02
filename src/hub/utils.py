fd125cce3e2754820c0b6d4dfe275633c04d359a
Toran Sahu <toran.sahu@yahoo.com>
2018-11-02 22:26:06 +0530

    [new] added util module; added jwt logic

---
+++ src/hub/utils.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-11-02 20:33
+
+"""
+utils.py
+"""
+
+
+import jwt
+
+
+__author__ = "Toran Sahu  <toran.sahu@yahoo.com>"
+__license__ = "Distributed under terms of the MIT license."
+
+
+SECRET = "ToPsEcReT"
+ALGORITHM = "HS256"
+
+
+def encode(payload):
+    """Encode to JWT"""
+    return jwt.encode(payload, SECRET, algorithm=ALGORITHM)
+
+
+def decode(token):
+    """Decode JWT"""
+    return jwt.decode(token, SECRET, algorithms=[ALGORITHM])
