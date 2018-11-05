ded635cbe2e7e3138ed6d9518c77c3c85ca5364f
Toran Sahu <toran.sahu@yahoo.com>
2018-11-05 11:22:38 +0530

    [update] cleaned unused codes

--- src/photogram/permissions.py
+++ src/photogram/permissions.py
@@ -10,38 +10,25 @@ permissions.py
 from rest_framework.permissions import BasePermission
 
 
-__author__ = 'Toran Sahu <toran.sahu@yahoo.com>'
-__license__ = 'Distributed under terms of the MIT license'
+__author__ = "Toran Sahu <toran.sahu@yahoo.com>"
+__license__ = "Distributed under terms of the MIT license"
 
 
-SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
-
-
-class IsUser(BasePermission):
-    """Owner and admin only will have all permissions."""
-    def has_object_permission(self, request, view, obj):
-        if request.user:
-            if request.user.is_superuser:
-                return True
-            else:
-                return obj == request.user
-
-        return False
+SAFE_METHODS = ["GET", "HEAD", "OPTIONS"]
 
 
 class IsAuthenticatedOrReadOnly(BasePermission):
     """The request is authenticated as a user, or is a read-only requ  est.
     """
+
     message = "[warn] - Unauthorized Access"
 
     def has_permission(self, request, view):
-        if (request.method in SAFE_METHODS
-                and request.query_params.get('token')):
-            # TODO: check for valid token
-            if(True):
-                return True
-            return False
-        if (request.user and request.user.is_authenticated) or request.user.is_superuser or request.user.is_staff:
+        if (
+            (request.user and request.user.is_authenticated)
+            or request.user.is_superuser
+            or request.user.is_staff
+        ):
             return True
         return False
 
